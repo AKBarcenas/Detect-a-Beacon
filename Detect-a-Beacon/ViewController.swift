20e2ec6c42fdbc8aecca00e1df7fa73770731ae9
AKBarcenas <akbarcen@ucsd.edu>
2016-06-28 06:07:25 -0700

    Allow the app to start scanning for the beacon.

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -12,9 +12,17 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     // The label that display the distance reading for the beacon.
     @IBOutlet weak var distanceReading: UILabel!
-    
+    // The location manager for the app.
     var locationManager: CLLocationManager!
 
+    /*
+     * Function Name: viewDidLoad
+     * Parameters: None
+     * Purpose: This method sets up the location manager and request for location authorization.
+     *   This method also sets the color of the background.
+     * Return Value: None
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,12 +38,66 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+     * Function Name: locationManager
+     * Parameters: manager - the location manager that is handling this method.
+     *   status - the new location authoriztion status of this app.
+     * Purpose: This method checks to see if location authorization has been given and starts scanning for beacons.
+     * Return Value: None
+     */
+    
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == .AuthorizedAlways {
             if CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
-                    
+                    startScanning()
                 }
+            }
+        }
+    }
+    
+    /*
+     * Function Name: startScanning
+     * Parameters: None
+     * Purpose: This method specifies what beacon the app will be looking for and relays that information to
+     *   the location manager.
+     * Return Value: None
+     */
+    
+    func startScanning() {
+        let uuid = NSUUID(UUIDString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoringForRegion(beaconRegion)
+        locationManager.startRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    /*
+     * Function Name: updateDistance
+     * Parameters: distance - the distance to the beacon.
+     * Purpose: This method checks the distance to the beacon and updates the background
+     *   and text of the app accordingly.
+     * Return Value: None
+     */
+    
+    func updateDistance(distance: CLProximity) {
+        UIView.animateWithDuration(0.8) { [unowned self] in
+            switch distance {
+            case .Unknown:
+                self.view.backgroundColor = UIColor.grayColor()
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .Far:
+                self.view.backgroundColor = UIColor.blueColor()
+                self.distanceReading.text = "FAR"
+                
+            case .Near:
+                self.view.backgroundColor = UIColor.orangeColor()
+                self.distanceReading.text = "NEAR"
+                
+            case .Immediate:
+                self.view.backgroundColor = UIColor.redColor()
+                self.distanceReading.text = "RIGHT HERE"
             }
         }
     }
