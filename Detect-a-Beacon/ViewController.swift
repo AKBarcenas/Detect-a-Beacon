40e8fdae32c1bbf63c685e2b813e5e1401190157
AKBarcenas <akbarcen@ucsd.edu>
2016-06-28 05:13:52 -0700

    Allow the app to ask for location information.

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    // The label that display the distance reading for the beacon.
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        view.backgroundColor = UIColor.grayColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways {
+            if CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
 }
 
